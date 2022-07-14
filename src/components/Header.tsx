091aba3ae7f45245b305646ce0e849d7d59212dc
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2022-07-14 22:32:41 +0900

    Header CSS 기본 구현 및 styled-components 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 50px;
+  width: 100%;
+  height: 85px;
+`;
+
+const Title = styled.h1`
+  margin: 1.5rem 1rem;
+`;
+
+const WriteBtn = styled.button`
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid #e0e0e0;
+  border-radius: 25px;
+  background: white;
+  color: #90a4ae;
+  width: 8rem;
+  height: 3rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+`;
 
 const Header = () => {
   const navigate = useNavigate();
 
   return (
     <>
-      <span>Mind Note</span>
-
-      <span
-        onClick={() => {
-          navigate(`/diary/new`);
-        }}
-      >
-        새 일기 쓰기
-      </span>
+      <HeaderContainer>
+        <Title>Mind Note</Title>
+        <WriteBtn
+          onClick={() => {
+            navigate(`/diary/new`);
+          }}
+        >
+          새 일기 쓰기
+        </WriteBtn>
+      </HeaderContainer>
     </>
   );
 };
